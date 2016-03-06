737e754dc3060398ea61f385d9c589cdabff075f
SongJLG <SongJL0818@outlook.com>
2016-03-07 00:23:42 +0800

    整理发布
    多处修改

--- Spider/IPy_Nb/TVPLSpider_TVMAO.py
+++ Spider/IPy_Nb/TVPLSpider_TVMAO.py
@@ -8,6 +8,26 @@ import xlrd
 import xlwt
 from xlutils.copy import copy
 
+def st():
+    print u"""
+    #------------------------------------------------------------------------
+    #   写着玩儿。请勿用于其他用途。如果觉得方便，可以请我喝酒。
+    #   Copyright Reserved by authors.
+    #   2.0.0版本。大改动，爬取源更换成电视猫,同时利用模板直接生成Excel。
+    #   如有建议，请发邮件 Sgang19890818@Gmail.com，或自行去GitHub修改源码。
+    #   A spider for Catch TV Progam List.
+    #   作者：老宋 平生多磨砺，男儿自横行。
+    #   https://github.com/SongJLG/TVPL_Spider/
+    #   Date: 2016-03-05
+    #-------------------------------------------------------------------------
+    """
+    
+    print u"你可以在命令行后输入数字(1-6)来选择频道"
+    print u"通过在命令行后键入help获取频道列表"
+    Select = str(raw_input())
+    mySpider = TVPL_Spider(Select)
+    mySpider.start()
+
 class TVPL_Spider:
     def __init__(self, Select):
         self.channel = {"1":u"翡翠台", "2":u"明珠台", "3":u"国际台", "4":u"本港台",
@@ -33,7 +53,7 @@ class TVPL_Spider:
         style_0 = xlwt.easyxf('pattern: pattern solid, fore_colour pale_blue; font: bold on;')        #两种写入Style
         style_1 = xlwt.easyxf('font: bold on;')
 
-        for i in range(8, 15):
+        for i in range(7, 14):                                                          #由于电视猫周日只更新到下周六，所以爬取本周日为第一天，截止到下周六
             postdata = urllib.urlencode({ 'email':'786755516@qq.com', 'pwd':'1q2w3e', 'ek':'MTQ1NzE1ODM5OTEzMQ=='})    #模拟登陆，绕开电视猫的反爬虫机制
             url = 'http://www.tvmao.com/program_favorite/' + self.ch_id[self.Select] + '-w' + str(i) + '.html'
             headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1;en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}  #伪装浏览器访问
@@ -56,15 +76,19 @@ class TVPL_Spider:
 
         new_wb.save(self.Done_xls[self.Select])             #保存Excel文件
                 
-        print u"爬虫抓取完成，不用谢。"
+        print u"爬虫抓取完成"
                                  
     def start(self):
         if self.Select == "help":
             for i in range(len(self.channel)):
-                print "%3d : %3s" % (i+1, self.channel["%s" % (i+1)]),
+                print "%3d : %3s" % (i+1, self.channel["%s" % (i+1)])
+            print ""
+            st()     
 
         elif self.Select not in self.channel.keys():
             print u"超出范围。请在1-6之间选择。"
+            print ""
+            st() 
         
         else:
             print u"你选择了"+"%3s" %self.channel[self.Select]
@@ -72,22 +96,5 @@ class TVPL_Spider:
             self.spider()
 
 if __name__=="__main__":
-    print u"""
-    #------------------------------------------------------------------------------
-    #   写着玩儿，出问题概不负责，最好爬完之后更官网粗略核对。
-    #   请勿用于其他用途。觉得方便，可以请我喝酒。
-    #   Copyright Reserved by authors.
-    #   2.0.0版本。大改动，爬取源更换成电视猫,同时利用模板直接生成Excel。
-    #   如有建议，请发邮件 Sgang19890818@Gmail.com，或自行去GitHub修改源码。
-    #   A spider for Catch TV Progam List.
-    #   作者：老宋 平生多磨砺，男儿自横行。
-    #   https://github.com/SongJLG/TVPL_Spider/
-    #   Date: 2016-03-05
-    #------------------------------------------------------------------------------
-    """
-      
-    print u"你可以在命令行后输入数字(1-6)来选择频道 "
-    print u"通过在命令行后键入help获取频道列表"
-    Select = str(raw_input())
-    mySpider = TVPL_Spider(Select)
-    mySpider.start()
+    st()
+    
